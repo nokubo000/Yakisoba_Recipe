2abcfeaf389f42f04486f5c43d6ca8749dfbd5d6
nokubo000 <105145174+nokubo000@users.noreply.github.com>
2022-05-08 00:04:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * Name: Nina Okubo
+ * Date: October 16th 2021
+ * Section: AC, TAs: Taylor Gotfrid and Ludvig Liljenberg
+ *
+ * This is the JS to implement the UI for my Creative Project 2 - Yakisoba Recipe.
+ * It takes the user to different parts of the recipe based on user input.
+ */
+"use strict";
+
+(function() {
+
+  window.addEventListener("load", init);
+
+  /**
+   * This starts running when the page initially loads.
+   * Allowing buttons and pictures to respond once they have been clicked on
+   */
+  function init() {
+    document.getElementsByClassName("yakisoba")[0].addEventListener("click", addPics);
+    id("to-follow-along").addEventListener("click", function() {
+      changeVis(id("ingre-list"));
+    });
+    id("to-prep").addEventListener("click", function() {
+      changeVis(id("ingre-prep"));
+    });
+    id("to-start-cooking").addEventListener("click", function() {
+      changeVis(id("start-cooking"));
+    });
+  }
+
+  /**
+   * Adds three pictures of Yakisoba everytime the first yakisoba
+   * picture is clicked on.
+   */
+  function addPics() {
+    for (let i = 0; i < 3; i++) {
+      let newImg = document.createElement("img");
+      newImg.src = 'https://live.staticflickr.com/4057/4716936691_f8f46aca80_b.jpg';
+      newImg.alt = "Close-up of Yakisoba";
+      let parent = id("yakisoba-pics");
+      parent.appendChild(newImg);
+      newImg.classList.add("yakisoba");
+    }
+  }
+
+  /**
+   * Changes the visibility of the given element's ID to the opposite
+   * visibility from when the button was initially clicked.
+   * (Turns invisible elements to be visible, and visible elements to be invisible)
+   * @param {string} recipeSection - ID of the element the
+   * user wants to change the visibility of.
+   */
+  function changeVis(recipeSection) {
+    if (recipeSection.style.display === "none") {
+      recipeSection.style.display = "block";
+    } else {
+      recipeSection.style.display = "none";
+    }
+  }
+
+  /** ------------------------------ Helper Functions  ------------------------------ */
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+})();
